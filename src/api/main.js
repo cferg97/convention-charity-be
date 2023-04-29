9ec90a618cd2bddabd6fb9ad8cf96226a8c18ed9
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-04-30 00:16:42 +0100

    tyjgh

--- src/api/main.js
+++ src/api/main.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { get } from "mongoose";
 import submitModel from "./model.js";
 import express from "express";
 import multer from "multer";
@@ -6,6 +6,8 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 import { getPDFReadableStream } from "../lib/pdf-tools.js";
 import { pipeline } from "stream";
+import nodemailer from "nodemailer";
+import sendgridTransport from "nodemailer-sendgrid-transport";
 
 const mainRouter = express.Router();
 
@@ -26,11 +28,31 @@ mainRouter.post("/", cloudinaryUpload, async (req, res, next) => {
     });
     const finalSub = await submission.save();
 
-    res.setHeader("Content-Disposition", "attachment; filename=item_sub.pdf");
-    const source = await getPDFReadableStream(finalSub);
-    const destination = res;
-    pipeline(source, destination, (err) => {
-      if (err) console.log(err);
+    const transporter = nodemailer.createTransport(
+      sendgridTransport({
+        auth: {
+          api_key: process.env.SG_API,
+        },
+      })
+    );
+
+    const pdf = await getPDFReadableStream(finalSub);
+
+    const mailOptions = {
+      from: "c.ferguson1997@gmail.com",
+      to: "c.ferguson1997@gmail.com",
+      subject: "New Charity Submission",
+      text: "There has been a new charity submission, see attached pdf.",
+      attachments: {
+        filename: "submission.pdf",
+        content: pdf,
+      },
+    };
+    transporter.sendMail(mailOptions, function (err) {
+      if (err) {
+        next(console.log(err));
+      }
+      res.status(201).send();
     });
   } catch (err) {
     next(err);
